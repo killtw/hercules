60cd7d845fc6725313a402fe4c0d3a6f822f56e7
Karl Li <killtw@gmail.com>
2021-07-26 18:25:00 +0800

    feat(server): remove layout from template

--- server/render.go
+++ server/render.go
@@ -12,19 +12,13 @@ type TemplateRegistry struct {}
 var LayoutTemplate = &TemplateRegistry{}
 
 func (t *TemplateRegistry) Render(w io.Writer, name string, data interface{}, ctx echo.Context) error {
-    layout := "base.html"
-    layoutInter := ctx.Get("layout")
-    if layoutInter != nil {
-        layout = layoutInter.(string)
-    }
-
     funcMap := template.FuncMap{
         "inc": func(i int) int {
             return i + 1
         },
     }
 
-    tmpl, err := template.New(layout).Funcs(funcMap).ParseFiles("views/" + layout, "views/" + name)
+    tmpl, err := template.New(name).Funcs(funcMap).ParseFiles("views/" + name)
     if err != nil {
         err := errors.New("Template not found -> " + name)
 
